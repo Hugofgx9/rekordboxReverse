fb4f581dd73017fe7974eb842a18b8ac0a818df5
Hugofgx9 <hugo.faugeroux@mmibordeaux.com>
2021-04-13 12:26:57 +0200

    spe chars transform

--- src/js/index.js
+++ src/js/index.js
@@ -9,17 +9,39 @@ $button.addEventListener('click', () => {
 });
 
 
+//fs.mkdirSync('/Users/hugofaugeroux/hello', { recursive: true });
+
+
 ipc.on('selectedElement', (event, path) => {
   fs.readFile(path.filePaths[0], 'utf8', function(err, data) {
     let json = convert.xml2json(data, {compact: true, spaces: 4});
     let resp = JSON.parse(json);
 
     let playlists = resp.DJ_PLAYLISTS.PLAYLISTS.NODE.NODE;
-    // playlists.forEach( (el) => {
-    //   console.log(el);
-    // });
-    firstOrCreate('playlist');
-    playlistTraitement(playlists, 'playlist');
+    let tracks = resp.DJ_PLAYLISTS.COLLECTION.TRACK;
+
+    let counter = 0;
+
+    tracks.forEach(  track => {
+
+      //let track = tracks[100];
+
+      let path = track._attributes.Location;
+      path = path.replace(new RegExp('.+?(?=\/Users)'), ''); //remove le début
+      path = decodeURIComponent(path); //special chars from utf8 to chars
+
+      //si le fichier existe 
+      if (fs.existsSync(path)) {
+        counter++;
+      } 
+      //le fichier n'existe pas 
+      else {
+      }
+
+    });
+    //playlistTraitement(playlists, 'playlist');
+
+    console.log(`${counter}/${tracks.length} files found`);
 
 
   });
@@ -27,28 +49,30 @@ ipc.on('selectedElement', (event, path) => {
 
 
 function playlistTraitement(playlistArray, parent) {
-  console.log(parent);
 
-  for (const i in playlistArray) {
-    //console.log(playlistArray[i]);
-
-    if ( playlistArray[i]._attributes ) {
-      const correctFolderName = playlistArray[i]._attributes.Name.replace('/', '_');
-      const path = `${parent}/${correctFolderName}`;
-      // if array is a playlist
-      if ( playlistArray[i]._attributes.Type == 1) {
-        //console.log(`Playlist ${path}`);
-        firstOrCreate(path);
-      }
-      
-      //if obj is a folder -> recurrence
-      else if ( playlistArray[i]._attributes.Type == 0 ) {
-        //console.log(`FOLDER ${parent}/${playlistArray[i]._attributes.Name}`);
-        //firstOrCreate(path);
-        playlistTraitement(playlistArray[i].NODE, path);
+  function createFolders(playlistArray, parent) {
+    for (const i in playlistArray) {
+  
+      if ( playlistArray[i]._attributes ) {
+  
+        const correctFolderName = playlistArray[i]._attributes.Name.replace('/', '_');
+        const path = `${parent}/${correctFolderName}`;
+  
+        // if array is a playlist
+        if ( playlistArray[i]._attributes.Type == 1) {
+          firstOrCreate(path);
+        }
+        
+        //if obj is a folder -> recurrence
+        else if ( playlistArray[i]._attributes.Type == 0 ) {
+          firstOrCreate(path);
+          createFolders(playlistArray[i].NODE, path);
+        }
       }
     }
+
   }
+
 }
 
 
